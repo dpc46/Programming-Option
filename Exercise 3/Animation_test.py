39c606b00f1e0b56ddebee8951360be740deb261
Dan Cochrane <danpcochrane@gmail.com>
2021-04-16 12:19:32 +0100

    Added description and changed the arguements

---
+++ Exercise 3/Animation_test.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+fig, ax = plt.subplots()        #defining a single plot
+xdata, ydata = [], []           #defining empty lists for xdata, ydata
+ln, = plt.plot([], [], 'ro')    #plots x,y data when called
+
+def init():
+    ax.set_xlim(0, 10)     #defining the axes properly
+    ax.set_ylim(0, 10)
+    return ln,             #return exits a function and returns a value
+
+def update(x):              #defining a function update, with arguement frame
+    xdata.append(x)         #adding x data point to xdata
+    ydata.append(x)         #adding y data point to ydata
+    ln.set_data(xdata, ydata)
+    return ln,                  #returns function with extra points added
+
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 10, 100),
+                    init_func=init, blit=True, interval=50)
+
+
+
+
+plt.show()
